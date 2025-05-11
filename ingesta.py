9c03c7e6601738ada340a81b6c1ea03c0a9b278d
Fedyboq <166071546+Fedyboq@users.noreply.github.com>
2025-05-11 11:01:56 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,9 @@
 import boto3
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+ficheroUpload = "facturas.json"
+nombreBucket = "facturas-cloud-pp"
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+response = s3.upload_file(ficheroUpload, nombreBucket, "facturas/facturas.json")
 
 print("Ingesta completada")
